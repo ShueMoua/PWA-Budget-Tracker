ecfdccabd4b53aae6303ee4c0d3a7fd4fb6fbabf
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-04 16:52:29 -0600

    added files to cache

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "./index.html",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
+    "./index.js",
+    "./manifest.webmanifest",
+    "./styles.css",
+];
+
